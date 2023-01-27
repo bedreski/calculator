f245fbdc22df9c7a269282a4ab588c4601efdd69
bedreski <fabianabedreski@gmail.com>
2023-01-27 14:07:46 -0300

    create 'result' variable to handle operation result

--- app.rb
+++ app.rb
@@ -2,15 +2,16 @@
 
 require 'sinatra'
 require 'erb'
-require_relative 'models/calculator.rb'
+require_relative 'models/calculate_by_operator.rb'
 class App < Sinatra::Base
   get '/' do
     erb :index
   end
 
   post '/' do
-    @calculate = CalculateByOperator.new params[:number1, :number2, '+']
+    @calculate = CalculateByOperator.new params[:number1, :number2, :operations]
+    @result = @calculate.calculate 
     erb :index
-    redirect '/'
+    # redirect '/'
   end
 end
